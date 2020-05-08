6566e91913882e49e664a0314c5bb082a3bade88
Mohammad Yalaan <mohamadyalaan@gmail.com>
2020-05-08 16:49:09 +0430

    some changing

--- scripts/app.js
+++ scripts/app.js
@@ -1,54 +1,25 @@
-const catList = document.getElementById('cat-list');
-const asideSec = document.getElementById('result');
-const showSection = document.getElementById('show-info');
+const catList = document.getElementById("cat-list");
+const asideSec = document.getElementById("result");
+const showSection = document.getElementById("show-info");
+
+let resId = [];
+let count = 0;
 
 async function getCategories() {
   let categories;
-  fetch(
-    "https://developers.zomato.com/api/v2.1/categories",
-    {
-      headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" },
-    }
-  ).then(res => res.json())
-    .then(cats => categories = cats)
+  fetch("https://developers.zomato.com/api/v2.1/categories", {
+    headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" },
+  })
+    .then((res) => res.json())
+    .then((cats) => (categories = cats))
     .then(() => renderCategories(categories));
 }
 
-
-async function fetchSearch() {
-  let restaurants;
-  fetch(
-    "https://developers.zomato.com/api/v2.1/search",
-    {
-      headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" },
-    }
-  ).then(res => res.json())
-    .then(results => restaurants = results)
-    .then(() => renderSearch(restaurants));
-}
-
-async function fetchShowSelected() {
-  let restaurants;
-  fetch(
-    "https://developers.zomato.com/api/v2.1/search",
-    {
-      headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" },
-    }
-  ).then(res => res.json())
-    .then(results => restaurants = results)
-    .then(() => renderShowSelected(restaurants));
-}
-
-
 function renderCategories(cats = []) {
-
   const { categories } = cats;
   for (let category of categories) {
     const {
-      categories: {
-        id,
-        name
-      }
+      categories: { id, name },
     } = category;
     const elmts = `<div data-id="${id}">
         <p>${name}</p>
@@ -57,45 +28,69 @@ function renderCategories(cats = []) {
   }
 }
 
+async function fetchSearch() {
+  let restaurants;
+  fetch("https://developers.zomato.com/api/v2.1/search", {
+    headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" },
+  })
+    .then((res) => res.json())
+    .then((results) => (restaurants = results))
+    .then(() => renderSearch(restaurants));
+}
 
-function renderSearch(searchs = []) {
-  
-  const {restaurants} = searchs;  
-  for(let rest of restaurants){
-    const {
-      restaurant:{
-        id,
-        name
-      }
-    } = rest;
-    const elmts = `<div id="left-items" data-id="${id}">
-    <p>${name}</p>
-</div>`;
-asideSec.innerHTML = asideSec.innerHTML + elmts;
-    // console.log(searchs);
-  }
+async function fetchShowSelected() {
+  let secrestaurants;
+  fetch("https://developers.zomato.com/api/v2.1/restaurant?res_id=19360573", {
+    headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8"},"res_id":resId
+     
+  })
+    .then((res) => res.json())
+    .then((results) => (secrestaurants = results))
+    .then(() => renderShowSelected(secrestaurants));
 }
 
 function renderShowSelected(searchs = []) {
-  
-  const {restaurants} = searchs;
-  for(let rest of restaurants){
-    const
-    {restaurant:{photos}
-  } = rest;
-    console.log(photos);
-//     const elmts = `<div data-id="${id}">
-//      <img class=""show-image-one" srs="${url}"/>
-// </div>`;
-// showSection.innerHTML = showSection.innerHTML + elmts;
+  // console.log("new is "+searchs);
+  const { restaurant } = searchs;
+  for (let restt of restaurant) {
+    const {
+      restaurant: {
+        photos,
+        R
+      }
+    } = restt;
+    // console.log(R);
+    //     const elmts = `<div data-id="${id}">
+    //      <img class=""show-image-one" srs="${url}"/>
+    // </div>`;
+    // showSection.innerHTML = showSection.innerHTML + elmts;
     // console.log(featured_image);
   }
 }
 
 
+
+async function renderSearch(searchs = []) {
+  const { restaurants } = searchs;
+  for (let rest of restaurants) {
+    const {
+      restaurant: {
+        id,
+        name,
+        R: { res_id },
+      },
+    } = rest;
+    resId[0] = res_id;
+    const elmts = `<div id="left-items" data-id="${res_id}">
+    <p>${name}</p>
+</div>`;
+    asideSec.innerHTML = asideSec.innerHTML + elmts;
+    console.log(searchs);
+  }
+  // console.log(resId);
+}
+
+
 window.addEventListener("load", getCategories);
-window.addEventListener("load",fetchSearch);
-window.addEventListener("load",fetchShowSelected);
-
-
-
+window.addEventListener("load", fetchSearch);
+window.addEventListener("load", fetchShowSelected);
